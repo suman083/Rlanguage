072e218d85191e8f2eaba83d757d1e17fbfd70bb
Anurag Ranjan <anuragranjan330@gmail.com>
2020-10-03 09:19:32 +0530

    cmmit

--- 1stProgram.R
+++ 1stProgram.R
@@ -10,3 +10,19 @@ x=c(3,5,6);
 x
 ls();
 objects();
+x[2]
+?seq()
+seq()
+seq(3,8)
+seq(from=2, length=4)
+seq(from=4, to=8)
+seq(from=3, to=8, by=.05)
+paste(1:5)
+class(paste(1:4))
+paste("xyz", 1:10)
+paste("xyz",c(2,5,7,"test",4.5))
+paste("xyz",1:10,sep="")
+?rep
+rep(c(1,2,5),3)
+x=c(1,2,5,7);
+rep(x,rech=2,times=3)
